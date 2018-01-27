932720291aa4c923365a951c169fca359ca069e7
Austin Green <austinxgreen@gmail.com>
2018-01-27 13:49:01 -0600

    usaddress, geopy, began advanced parsing of address from raw text

--- bham_murders/strip_address.py
+++ bham_murders/strip_address.py
@@ -5,27 +5,59 @@
 # https://www.bhamwiki.com/w/List_of_Birmingham_homicides_in_2017
 
 from geopy.geocoders import Nominatim
+import usaddress as usa
 
 geolocater = Nominatim()
 
 fn = "2017_raw.txt"
-city_state = "birmingham AL"
-
+city_name = "Birmingham"
+state_name = "AL"
+garbage_words = [" of ", " in ", " the "]
+address_strings = []
 
 with open(fn) as f:
     total_sep = 0
     row_count = 0
     for i, row in enumerate(f):
         row_count += 1
-        location = row.split("on the")
+        location = row.split("on the ")
         if len(location) == 1:
-            location = row.split("at the")
+            location = row.split("at the ")
         if len(location) == 1:
             location = row.split(" at ")
         if len(location) == 1:
-            print(location)
-        total_sep += len(location)
+            location = row.split(" on ")
+        if len(location) == 1:
+            print("Irregular formatting, must manually format entry number : ", i)
+        else:
+            location = location[1]
+            if "." in location:
+                location = location.split(".")[0]
+            for word in garbage_words:
+                location = location.replace(word, " ")
+        address_strings.append(location)
 
-    print("PrSeparations : ", (total_sep/row_count) - 1)
-    print("Lines in file : ", row_count)
-    print("separations : ", total_sep)
+
+with open("bham_murder_addresses.csv", "a") as fn:
+    """ take the address strings, and format them in a way that will be 
+        understandable by geopy.  best case is 
+        address number streetname city state"""
+    usaddress_keys = ["AddressNumber", "StreetName",
+                    'StreetNamePostType', 'StreetNamePostDirectional']
+    address_details = []
+    refined_addresses = []
+    for i, address in enumerate(address_strings):
+        details = usa.tag(address)[0]
+
+        # create an entry so there aren't errors if address is unparsable, or
+        # some other error
+        refined_addresses.append("")
+        # Build ideal addresses
+        for key in usaddress_keys:
+            if key in details:
+                refined_addresses[i] += details[key]
+
+            refined_addresses[i] += f', {city_name}, {state_name}'
+
+    for ra in refined_addresses:
+        print(refined_addresses)
